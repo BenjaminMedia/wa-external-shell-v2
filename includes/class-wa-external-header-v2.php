ca170845cc869036a24a5b7cb6ddbd97d369608f
Frederik Rabøl <frederik-rm@hotmail.com>
2017-09-15 09:17:29 +0200

    added two new settings fields I've added the following new settings: - checkmark for the new compact menu functionality in the WA Shell - a field to override the wp_head hook, in case you want to change the position of the shell.

--- includes/class-wa-external-header-v2.php
+++ includes/class-wa-external-header-v2.php
@@ -188,8 +188,9 @@ class Wa_External_Header_V2 {
 
 		$plugin_public = Wa_External_Header_V2_Public::getInstance( $this->get_plugin_name(), $this->get_version(), $this->get_options_group_name() );
 
+		$headHook = (isset($plugin_public->getUserConfig()['head_hook']) ? $plugin_public->getUserConfig()['head_hook'] : "wp_head");
 		$this->loader->add_action( 'wp_enqueue_style', $plugin_public, 'enqueue_fontawesome_styles' );
-		$this->loader->add_action( 'wp_head', $plugin_public, 'wp_head',40);
+		$this->loader->add_action( $headHook, $plugin_public, 'wp_head', 40);
 		$this->loader->add_action( 'wp_footer', $plugin_public, 'wp_footer' );
 
 	}
